3ee9fe4ab76eea4c17fdf096253948dae099c1b8
Puraumu <puraumu@macpro.local>
2012-08-14 00:25:07 +0900

    parser: todo => error test and eval, Info: todo => environment

--- test/parser.js
+++ test/parser.js
@@ -3,8 +3,9 @@ var Getter = require('../')
   , should = require('should')
   , express = require('express')
   , app = express()
+  , fs = require('fs')
+  , join = require('path').join
   , host = 'http://localhost:3003'
-  , parser;
 
 app.get('/', function(req, res){
   res.send('foobar');
@@ -15,77 +16,64 @@ app.get('/foo', function(req, res){
 });
 
 app.get('/hoge', function(req, res){
-  res.send('here is content');
+  res.send("here is content\n");
 });
 
 app.listen(3003);
 
-var set = {
-      jquery: 'foobar',
-      proxy: ['foo', 'bar', 'hoge'],
-      url: host + '/' };
+var act = {}, set = {}, parser, out = join(__dirname, '../sd')
+act.fn = function() {  }
+act.cookie = []
+act.url = host + '/'
+act.withproxy = false
+act.mode = 'get'
 
-describe('Parser', function(){
+set.proxy = [{data:'foo'}, {data:'bar'}, {data:'hoge'}]
+set.dldir = out
+set.retry = false
 
-  describe('new', function(){
-    it('should be initialized', function(){
-      parser = new Parser(set)
-      parser.title.should.be.empty
-      parser._url.should.eql(host + '/')
+describe('Parser', function() {
+
+  describe('new', function() {
+    it('should be initialized', function() {
+      parser = Parser(set)
+      parser.proxyList.should.have.length(3)
     })
-  })
-
-  describe('pushAct()', function(){
-    it('should invoke fn #1', function(done){
-      var d = {
-        cookie: '',
-        url: host + '/',
-        withproxy: false,
-        mode: 'get',
-        fn: function(body, url, n){
-          body.should.eql('foobar');
-          done()
-        } }
-      parser.pushAct(d)
+    it('should read dldir', function() {
+      parser.dldir.should.eql(out)
+    })
+    it('should read retry', function() {
+      parser.retry.should.eql(false)
     })
   })
 
   describe('request()', function(){
     it('should get 200', function(done){
-      var d = {
-        cookie: '',
-        url: host + '/',
-        withproxy: false,
-        mode: 'get',
-        fn: function(body, url){
+      act.fn = function(body, url, cwp) {
           body.should.eql('foobar');
-          done()
-        } }
-      parser.request(d)
+          done() }
+      parser.request(act, 0)
     })
     // it('should with proxy', function(){
     // })
     it('should get body and write it', function(done){
-      parser.title = 'foo-hoge'
-      var d = {
-        cookie: '',
-        url: host + '/hoge',
-        withproxy: false,
-        mode: 'write',
-        fn: function(){} }
-      parser.request(d)
+      act.mode = 'write'
+      act.fn = function() {}
+      act.url = host + '/hoge'
+      parser.request(act, 1)
       done()
     })
-    it('should write file with name current date', function(done){
-      parser.title = ''
-      var d = {
-        cookie: '',
-        url: host + '/hoge',
-        withproxy: false,
-        mode: 'write',
-        fn: function(){} }
-      parser.request(d)
-      done()
+  })
+
+  describe('.keepHeader()', function() {
+    it('should wait', function (done) {
+      setTimeout(function() { done() }, 5)
+    })
+    it('should keep response headers', function() {
+      parser.resHeaders.should.have.length(2)
+    })
+    it('should keep request headers', function() {
+      parser.reqHeaders.should.have.length(2)
     })
   })
 
@@ -105,54 +93,59 @@ describe('Parser', function(){
     })
   })
 
-  describe('`error`', function(){
-    it('should get 404 and details', function(done){
-      parser.once('error', function(){
-        parser.errors.should.have.length(1)
-        done()
-      })
-      var d = {
-        cookie: '',
-        url: host + '/hogehoge',
-        withproxy: false,
-        mode: 'get',
-        fn: function(body, url){ parser.emit('error') } }
-      parser.request(d)
-    })
-    it('should invoke function with argument null', function(done){
-      var d = {
-        cookie: '',
-        url: 'hogehoge',
-        withproxy: false,
-        mode: 'get',
-        fn: function(body, url, n){ should.strictEqual(body, null);done() } }
-      parser.request(d)
-    })
-    it('should not write response when not found', function(done){
-      parser.once('notfound', function(){
-        parser.errors.should.have.length(3)
-        done()
-      })
-      var d = {
-        cookie: '',
-        url: 'boboobobd',
-        withproxy: false,
-        mode: 'write',
-        fn: function(){ throw 'boooom!!' } }
-      parser.request(d)
+  describe('.evalProxy()', function() {
+    it('should', function() {
     })
   })
 
-  describe('`end`', function(){
-    it('should write errors as json', function(done){
-      parser.once('end', function(){ done(); })
-      parser.emit('end', 'file')
-    })
-    it('should put errors to stdout', function(done){
-      parser.once('end', function(){ done(); })
-      parser.emit('end', 'log')
-    })
-  })
+  // describe('`error`', function(){
+    // it('should get 404 and details', function(done){
+      // parser.once('error', function(){
+        // parser.errors.should.have.length(1)
+        // done()
+      // })
+      // var d = {
+        // cookie: '',
+        // url: host + '/hogehoge',
+        // withproxy: false,
+        // mode: 'get',
+        // fn: function(body, url){ parser.emit('error') } }
+      // parser.request(d)
+    // })
+    // it('should invoke function with argument null', function(done){
+      // var d = {
+        // cookie: '',
+        // url: 'hogehoge',
+        // withproxy: false,
+        // mode: 'get',
+        // fn: function(body, url, n){ should.strictEqual(body, null);done() } }
+      // parser.request(d)
+    // })
+    // it('should not write response when not found', function(done){
+      // parser.once('notfound', function(){
+        // parser.errors.should.have.length(3)
+        // done()
+      // })
+      // var d = {
+        // cookie: '',
+        // url: 'boboobobd',
+        // withproxy: false,
+        // mode: 'write',
+        // fn: function(){ throw 'boooom!!' } }
+      // parser.request(d)
+    // })
+  // })
+
+  // describe('`end`', function(){
+    // it('should write errors as json', function(done){
+      // parser.once('end', function(){ done(); })
+      // parser.emit('end', 'file')
+    // })
+    // it('should put errors to stdout', function(done){
+      // parser.once('end', function(){ done(); })
+      // parser.emit('end', 'log')
+    // })
+  // })
 
 })
 
