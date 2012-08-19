cdcc424c79008ff08af7f425461faf21ab77de07
Puraumu <puraumu@macpro.local>
2012-08-19 22:05:34 +0900

    options test

--- test/bot-request.js
+++ test/bot-request.js
@@ -67,33 +67,5 @@ describe('Bot', function() {
     })
   })
 
-  /**
-   * comment
-  describe('readProxy()', function(){
-    it('should read first item', function(){
-      client._proxy = -1;
-      client.readProxy(false).should.eql('foo')
-    })
-    it('should read second item', function(){
-      client.readProxy(false).should.eql('bar')
-    })
-    it('should read third item', function(){
-      client.readProxy(false).should.eql('hoge')
-    })
-    it('should read first item', function(){
-      client.readProxy(false).should.eql('foo')
-    })
-  })
-
-  describe('.evalProxy()', function() {
-    it('should return true, because 1st request was 200', function() {
-      client.evalProxy(0).should.be.true
-    })
-    it('should return true, because 2nd request was 200', function() {
-      client.evalProxy(1).should.be.true
-    })
-  })
-   */
-
 })
 
---
+++ test/options.js
@@ -0,0 +1,84 @@
+var root = require('../lib')
+  , options = root.options
+  , join = require('path').join
+  , fs = require('fs')
+  , should = require('should')
+
+var options
+  , url = 'http://localhost'
+  , cookie = {foo: 'bar', 'hoge': 45}
+
+describe('`options`', function() {
+
+  describe('initialize', function() {
+    it('should read arguments', function() {
+      options = options(url)
+      options.url.should.eql(url)
+    })
+  })
+
+  describe('stringify()', function() {
+    it('should', function() {
+      options.stringify(cookie).should.eql('foo=bar;hoge=45')
+    })
+  })
+
+  describe('addCookie()', function() {
+    it('should add cookie string', function() {
+      options.addCookie('hoge=baz')
+      options.jar.cookies.should.have.length(1)
+    })
+    it('should add cookie string', function() {
+      options.addCookie('ff=hh')
+      options.jar.cookies.should.have.length(2)
+    })
+    it('should add Array', function() {
+      options.addCookie(['e=bar', 'foo=bar'])
+      options.jar.cookies.should.have.length(4)
+    })
+  })
+
+  describe('isbinary()', function() {
+    it('should return false if request to get html', function() {
+      options.isbinary().should.be.false
+    })
+    it('should return true if request to binary object', function() {
+      options.url = 'http://localhost/foo.gif'
+      options.isbinary().should.be.true
+    })
+  })
+
+  describe('load()', function() {
+    it('should load proxy list', function() {
+      options.load(['foo', 'bar', 'hoge'])
+      options.proxyList.should.have.length(3)
+      options.disable('random')
+    })
+  })
+
+  describe('readProxy()', function(){
+    it('should read first item', function(){
+      options.readProxy().should.eql('foo')
+    })
+    it('should read second item', function(){
+      options.readProxy().should.eql('bar')
+    })
+    it('should read third item', function(){
+      options.readProxy().should.eql('hoge')
+    })
+    it('should back to first item', function(){
+      options.readProxy().should.eql('foo')
+    })
+  })
+
+  // describe('.evalProxy()', function() {
+    // it('should return true, because 1st request was 200', function() {
+      // options.evalProxy(0).should.be.true
+    // })
+    // it('should return true, because 2nd request was 200', function() {
+      // options.evalProxy(1).should.be.true
+    // })
+  // })
+
+})
+
