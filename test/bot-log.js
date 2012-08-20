bed8696ec5ca73bb214dded85e73e55da3aeb2aa
Puraumu <puraumu@macpro.local>
2012-08-20 20:05:44 +0900

    merge bot-event to bot-log

--- test/bot-log.js
+++ test/bot-log.js
@@ -42,5 +42,53 @@ describe('Bot-Log', function() {
     })
   })
 
+  describe('event', function() {
+    it('should listen request event', function(done) {
+      act.fn = function() {};
+      log.once('req', function() {
+        log.req.path.should.eql('/')
+        done()
+      })
+      client.set('url', host + '/')
+      client.trigger(act)
+    })
+    it('should listen response event', function(done) {
+      log.once('res', function() {
+        log.res.statusCode.should.eql(200)
+        done()
+      })
+      client.trigger(act)
+    })
+    it('should listen notfound event', function(done) {
+      log.once('notfound', function() {
+        log.req.exist.should.false
+        done()
+      })
+      client.set('url', 'http://localhost:9090/')
+      client.trigger(act)
+    })
+  })
+
+  describe('`error`', function(){
+    it('should treat it to be exist if request gets 404', function(done){
+      act.fn = function(body, url) {
+        log.res.statusCode.should.eql(404)
+        log.req.exist.should.be.true
+        done()
+      }
+      client.set('url', host + '/hogehoge')
+      client.trigger(act)
+    })
+    it('should not treat it to be exist if server was not found', function(done){
+      act.fn = function(body, url) {
+        should.strictEqual(body, null)
+        log.req.exist.should.be.false
+        done()
+      }
+      client.set('url', 'http://localhost:9090/')
+      client.trigger(act)
+    })
+  })
+
 })
 
