1fb411572c6abf32b48cf6a35990ceab67bc5c57
Puraumu <puraumu@macpro.local>
2012-08-21 10:50:57 +0900

    bot-log interface

--- test/bot-log-head.js
+++ test/bot-log-head.js
@@ -24,50 +24,50 @@ describe('Bot.Log.head', function() {
     it('should return response headers', function(done) {
       client = bot(set)
       client.set('url', host + '/')
-      act.fn = function(body, log) {
-        log.head.should.eql(log.res)
+      act.fn = function(res) {
+        res.head.should.eql(log.res)
         done()
       }
       client.trigger(act)
     })
     it('should return response statusCode', function(done) {
-      act.fn = function(body, log) {
-        log.status.should.eql(200)
+      act.fn = function(res) {
+        res.status.should.eql(200)
         done()
       }
       client.trigger(act)
     })
     it('should return response content-type', function(done) {
-      act.fn = function(body, log) {
-        log.type.indexOf('text/html').should.eql(0)
+      act.fn = function(res) {
+        res.type.indexOf('text/html').should.eql(0)
         done()
       }
       client.trigger(act)
     })
     it('should check if content-type is text/html', function(done) {
-      act.fn = function(body, log) {
-        log.html.should.be.true
+      act.fn = function(res) {
+        res.html.should.be.true
         done()
       }
       client.trigger(act)
     })
     it('should return if server exist', function(done) {
-      act.fn = function(body, log) {
-        log.exist.should.be.true
+      act.fn = function(res) {
+        res.exist.should.be.true
         done()
       }
       client.trigger(act)
     })
     it('should return response content-length', function(done) {
-      act.fn = function(body, log) {
-        log.length.should.be.a('number')
+      act.fn = function(res) {
+        res.length.should.be.a('number')
         done()
       }
       client.trigger(act)
     })
     it('should return request path', function(done) {
-      act.fn = function(body, log) {
-        log.path.should.eql('/')
+      act.fn = function(res) {
+        res.path.should.eql('/')
         done()
       }
       client.trigger(act)
