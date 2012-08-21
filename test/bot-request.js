261ee701d50b69e192d9510ba45eb18f9c473459
Puraumu <puraumu@macpro.local>
2012-08-21 22:02:07 +0900

    log test

--- test/bot-request.js
+++ test/bot-request.js
@@ -51,6 +51,42 @@ describe('Bot', function() {
       }
       client.trigger(false, 'test')
     })
+
+    /**
+     * prior bot.log
+  describe('`error`', function(){
+    it('should treat it to be exist if request gets 404', function(done){
+      client.actions.test = function() {
+        log.res.statusCode.should.eql(404)
+        log.req.exist.should.be.true
+        done()
+      }
+      client.set('url', host + '/hogehoge')
+      client.trigger(false, 'test')
+    })
+    it('should not treat it to be exist if server was not found', function(done){
+      client.actions.test = function(res) {
+        // should.strictEqual(res.body, null)
+        log.req.exist.should.be.false
+        done()
+      }
+      client.set('url', 'http://localhost:9090/')
+      client.trigger(false, 'test')
+    })
+  })
+
+  describe('time', function() {
+    it('should count response time', function(done) {
+      client.actions.test = function() {
+        log.duration.should.be.a('number')
+        done()
+      }
+      client.set('url', host + '/hogehoge')
+      client.trigger(false, 'test')
+    })
+  })
+     */
+
     /**
     it('should get body and write it', function(done){
       act.fn = function() {}
--- test/log.js
+++ test/log.js
@@ -1,78 +1,37 @@
-var root = require('../lib')
-  , bot = root.bot
+var root = require('../')
+  , Log = root.Log
+  , http = require('http')
   , should = require('should')
-  , express = require('express')
-  , app = express()
   , join = require('path').join
-  , host = 'http://localhost:3005'
 
-app.get('/', function(req, res){
-  res.send('foobar');
-});
-
-app.listen(3005);
+http.createServer(function(req, res) {
+  if (req.url == '/') {
+    res.end('foobar');
+  };
+}).listen(8989);
 
 function noop() {}
 
-var act = {}
-  , out = join(__dirname, '../sd')
-  , set = {dldir: out, actions: {}}
-  , client
-  , log = bot.log
+var log = Log()
+  , url = 'http://localhost'
+  , port = 8989
 
-describe('Bot.Log', function() {
+describe('Log', function() {
 
   describe('req', function() {
-    it('should have request header', function(done) {
-      client = bot(set)
-      client.actions.test = function(res, options) {
-        log.req.path.should.eql('/')
-        done()
-      }
-      client.set('url', host + '/')
-      client.trigger(false, 'test')
+    it('should have request header', function() {
+      log.keepHeader('req', http.request({url: url, port: port}))
+      log.req.path.should.eql('/')
     })
   })
 
   describe('res', function() {
     it('should have response header', function(done) {
-      client.actions.test = function(res, options) {
-        res.statusCode.should.eql(200)
+      http.get({url: url, port: port}, function(res) {
+        log.keepHeader('res', res)
+        log.res.statusCode.should.eql(200)
         done()
-      }
-      client.trigger(false, 'test')
-    })
-  })
-
-  describe('`error`', function(){
-    it('should treat it to be exist if request gets 404', function(done){
-      client.actions.test = function() {
-        log.res.statusCode.should.eql(404)
-        log.req.exist.should.be.true
-        done()
-      }
-      client.set('url', host + '/hogehoge')
-      client.trigger(false, 'test')
-    })
-    it('should not treat it to be exist if server was not found', function(done){
-      client.actions.test = function(res) {
-        // should.strictEqual(res.body, null)
-        log.req.exist.should.be.false
-        done()
-      }
-      client.set('url', 'http://localhost:9090/')
-      client.trigger(false, 'test')
-    })
-  })
-
-  describe('time', function() {
-    it('should count response time', function(done) {
-      client.actions.test = function() {
-        log.duration.should.be.a('number')
-        done()
-      }
-      client.set('url', host + '/hogehoge')
-      client.trigger(false, 'test')
+      })
     })
   })
 
