272314cb322460433ed2742cfb3ff5277dbcb988
Puraumu <puraumu@macpro.local>
2012-08-22 09:20:27 +0900

    action-request-error test

--- test/action-request-error.js
+++ test/action-request-error.js
@@ -1,31 +1,54 @@
+var robot = require('../')
+  , Action = robot.Action
+  , http = require('http')
+  , should = require('should')
+  , join = require('path').join
 
-  it('should listen notfound event', function(done) {
-    http.get({url: url, port: 9090})
-    log.once('notfound', function() {
-      log.req.exist.should.false
-      done()
-    })
-    client.set('url', 'http://localhost:9090/')
-    client.trigger(false, 'test')
-  })
+http.createServer(function(req, res) {
+  if (req.url == '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    return res.end('foobar');
+  };
+  res.writeHead(404, {'Content-Type': 'text/html'})
+  res.end('Not Found');
+}).listen(8989);
 
-  describe('`error`', function(){
+var action = new Action()
+  , host = 'http://localhost:8989'
+
+var out = join(__dirname, '../sd')
+  , set = {dldir: out}
+
+describe('Action.request.error', function() {
+
+  describe('`request`', function(){
     it('should treat it to be exist if request gets 404', function(done){
-      client.actions.test = function() {
-        log.res.statusCode.should.eql(404)
-        log.req.exist.should.be.true
+      action.set('url', host + '/hogehoge')
+      action.sendRequest(false, function() {
+        action.log.res.statusCode.should.eql(404)
+        action.log.req.exist.should.be.true
         done()
-      }
-      client.set('url', host + '/hogehoge')
-      client.trigger(false, 'test')
+      })
     })
     it('should not treat it to be exist if server was not found', function(done){
-      client.actions.test = function(res) {
+      action.set('url', 'http://localhost:9090/')
+      action.sendRequest(false, function() {
         // should.strictEqual(res.body, null)
-        log.req.exist.should.be.false
+        action.log.req.exist.should.be.false
         done()
-      }
-      client.set('url', 'http://localhost:9090/')
-      client.trigger(false, 'test')
+      })
     })
   })
+
+  describe('`event`', function() {
+    it('should listen notfound event', function(done) {
+      action.set('url', 'http://localhost:9090/')
+      action.sendRequest(false, function() {})
+      action.log.once('notfound', function() {
+        action.log.req.exist.should.false
+        done()
+      })
+    })
+  })
+
+})
