515258d0ec36ff042a541f36b41a5c622cf8ac6e
Puraumu <puraumu@macpro.local>
2012-08-22 09:50:51 +0900

    adjust server for multiple request test

---
+++ test/multiple.js
@@ -0,0 +1,8 @@
+var robot = require('../')
+  , Action = robot.Action
+  , join = require('path').join
+  , should = require('should')
+  , host = 'http://localhost:3002';
+
+var action = new Action();
+
